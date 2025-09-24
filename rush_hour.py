42190b6eaf791c6e60022e558c62cef4bb6784fc
KatyaL00 <k.lasskaya@gmail.com>
2025-09-24 18:12:37 +0100

    Implements a draw_road function to draw a black road centered on the canvas

--- rush_hour.py
+++ rush_hour.py
@@ -1,14 +1,27 @@
 import tkinter 
 
-WIDTH, HEIGHT = 600,600
+WIDTH, HEIGHT =800,600
 
 root = tkinter.Tk()
 root.title("Rush Hour") 
 root.resizable(False, False)
 
-
 canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg="pink")
-
 canvas.pack()  
+
+
+def draw_road(width):
+    road_x = WIDTH // 2 - width // 2
+    road = canvas.create_rectangle(
+        road_x, 
+        0, 
+        road_x + width, 
+        HEIGHT, 
+        fill="black"
+    )
+    return road   
+
+road = draw_road(220)
+
 
 root.mainloop()
