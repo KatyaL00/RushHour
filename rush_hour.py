b05bc9e9465a89beecb4fb2c9558276f57b8d9e7
KatyaL00 <k.lasskaya@gmail.com>
2025-10-04 14:03:03 +0100

    MADE CAR MOVE UP THE ROAD. Now the program shows the car's center x and y position each time it moves. This is done by finding the middle point between the car's sides and printing it.

--- rush_hour.py
+++ rush_hour.py
@@ -10,29 +10,37 @@ canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT, bg="pink")
 canvas.pack()  
 
 
+
+
 def draw_road(width):
-    road_x = WIDTH // 2 - width // 2
+    x0 = WIDTH // 2 - width // 2
+    y0 = 0
+
+    x1 = x0 + width
+    y1 = HEIGHT
+
     road = canvas.create_rectangle(
-        road_x, 
-        0, 
-        road_x + width, 
-        HEIGHT, 
+        x0, 
+        y0, 
+        x1, 
+        y1, 
         fill="black"
     )
+    return x0,y0,x1,y1
 
 road_width = 220
-draw_road(road_width)
+x0,y0,x1,y1 = draw_road(road_width)
 
-def draw_car():
+def draw_car(x0,y0,x1,y1):
     car_width = 30
     car_height = 40
-    road_x = WIDTH // 2 - road_width // 2
-    road_quater = road_width // 4
+    road_x = x0
+    road_quater = (x1 - x0) // 4
     car_centre = road_x + road_quater 
     car_x = car_centre - car_width // 2
-    car_y = HEIGHT - car_height
+    car_y = y1 - car_height
     car_x1 = car_centre + car_width // 2
-    car_y1 = HEIGHT 
+    car_y1 = y1 
 
 
     car = canvas.create_rectangle(
@@ -42,8 +50,21 @@ def draw_car():
         car_y1,
         fill="blue"
     )
+    return car
 
-draw_car()
+car = draw_car(x0,y0,x1,y1)
+
+def update():
+    car_pos = canvas.coords(car)
+    print("Car position:", car_pos)
+
+    if car_pos[3] > 0:
+        x0 = 0
+        y0 = -10
+
+        canvas.move(car, x0, y0)
+        root.after(50, update)
 
 
+update()
 root.mainloop()
